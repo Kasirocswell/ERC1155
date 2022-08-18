3518e4f9423a32afc1b70048e129dfb94edcbd43
Kasi Reeves <Kasirocswell@Rocketmail.com>
2022-08-18 09:56:19 -0700

    ERC1155 Example

---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  
+  const Items = await hre.ethers.getContractFactory("ERC1155");
+  const items = await Items.deploy("https://game.example/api/item/{id}.json");
+
+  await items.deployed();
+
+  console.log(
+    `ERC1155 Contract deployed to: `, items.address
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
